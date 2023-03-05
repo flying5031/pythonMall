30cb47fbb0f9e638ccb32f9b3423ca1020869554
flying5031 <flying_lx@qq.com>
2023-03-05 19:22:35 +0800

    testNumpyandPandas

--- code/temp/test.py
+++ code/temp/test.py
@@ -1,7 +1,11 @@
 import re
-
+import numpy
+import pandas as pd
 s = 'numpy  111'
 t = s.split('\s')
 z = re.split('\s+',s.strip())
 print(t)
 print(z)
+
+a = pd.Series(numpy.zeros(7+1) +1)
+print(a)
